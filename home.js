9b25f81bb67392877ac6a1e51651db55d9aecd96
Tyler Cheney <tyler.cheney.05@gmail.com>
2021-05-17 22:14:57 -0600

    module 1,2,3, part of 4

---
+++ home.js
@@ -0,0 +1,19 @@
+let welcome = 'Welcome',
+  name = 'Hiking Boots',
+  discounted = false,
+  price;
+
+price = 59.99
+price = 29.99;
+
+// show the title
+showMessage(price);
+
+// console.log("any message")
+/*
+  multi-line comments
+*/
+
+// let total = 149.99;
+// let product = 'Hiking Boots';
+// let discounted = true
